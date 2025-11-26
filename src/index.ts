53d1fa8f681966be7db52f825253483f8ddc8f9e
Camilo Barbosa <juanbedoya.barbosa@gmail.com>
2025-11-26 15:27:51 -0500

    refactor: move mount() into its own module

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export * from "./ref.js";
-export * from "./h.js";
-export * from "./router.js";
-export type * from "./types.js";
+export type { Props, Component, Child, Hole } from "./types.js";
+export { ref } from "./ref.js";
+export { h } from "./h.js";
+export { navigate, Link } from "./router.js";
+export { mount } from "./mount.js";
