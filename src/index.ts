4ab6d7863c0b2c0d3a529e239b5e5f307c4744dc
Camilo Barbosa <juanbedoya.barbosa@gmail.com>
2025-12-20 19:28:13 -0500

    export list() from /src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { ref } from "./public/ref.js";
 export { h } from "./public/h.js";
 export { navigate, Link } from "./public/router.js";
 export { mount } from "./public/mount.js";
+export { list } from "./public/list.js";
 export type { Props, Component, Child, Hole } from "./public/types.js";
